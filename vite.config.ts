c8c87ad2de6d325f988d9906cb4fb23f40dff072
south <fancustard9@gmail.com>
2025-01-01 17:03:16 +0900

    環境構築

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    hmr: {
-      protocol: "ws",
-      host: "localhost",
+    port: 5173, // 必要であればポート指定
+  },
+  build: {
+    rollupOptions: {
+      // 静的ファイルのパス設定
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
     },
   },
 });
